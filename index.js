460d244091a6bd332f5d293ebaf6668a5d8741a9
Kenneth <kennethdawes35@gmail.com>
2026-02-25 12:31:32 -0500

    added restart button

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ function random(word){
     return word[randomIndex];
 }
 function choosePhrase(){
-  phrase_list = ['vertical','temple','shark', 'lighter', 'divorce', 'lebron james', 'confusion', 'penetrate', 'minor', 'drake', 'spread', 'goat', 'horse', 'diddy']
+  phrase_list = ['vertical','temple','shark', 'lighter', 'lebron james', 'confusion', 'goat', 'horse']
   phrase_list = (random(phrase_list))
   console.log(phrase_list)
   return phrase_list
+}
+
+
+function hangman(){
+
+
+
+}
+
+function userCheck(){
+
+
+    
 }
