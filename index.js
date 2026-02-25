bac07be4bff16cdce31625aae241eec373f5d6eb
Layla <lwong6060@gmail.com>
2026-02-25 14:19:00 -0500

    Edited index js

--- index.js
+++ index.js
@@ -29,3 +29,22 @@ function sumbit_button(){
 
 
 
+def toDashes(list):
+  new_list = []
+  for letters in list:
+    if letters != " ":
+      new_list.append("_")
+    else:
+      new_list.append(" ")
+  return new_list
+
+def updateGameData(letter, dashes, phrase, guessLeft):
+  for i in range(len(phrase)):
+    if phrase[i] == letter:
+      dashes[i] = letter
+
+  if letter not in phrase:
+    guessLeft -=  1
+
+  return dashes, guessLeft
+
